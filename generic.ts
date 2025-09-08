bf8679444ef665f7af502fd445508b1a87aa3d7f
Samed C <57084553+samed-c@users.noreply.github.com>
2025-09-08 02:55:41 -0400

    Update

---
+++ generic.ts
@@ -0,0 +1,49 @@
+class Box<T>{
+    content:T;
+
+    constructor(par:T){
+        this.content = par;
+    }
+
+    openBox():T{
+        return this.content;
+    }
+}
+
+var bookBox = new Box<string>('Harry Potter');
+console.log(bookBox.openBox());
+
+var toyBox = new Box<number>(23424234);
+console.log(toyBox.openBox());
+
+
+
+
+function element<T, K extends keyof T>(parameter:T, par:K):T[K]{
+    return parameter[par];
+}
+
+const person = {age:19, name:'jack'}
+
+console.log(element<string,number>("Hello World",1));
+
+console.log(element(person, 'age'));
+
+
+
+
+interface HasLength{
+    length:number;
+}
+
+function checkLength<T extends HasLength>(item:T):void{
+    console.log(item.length);
+}
+
+checkLength([1,2,3]);
+
+
+
+
+type person = {name:string, age:number};
+type personKeys = keyof person;
